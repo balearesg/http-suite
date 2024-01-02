8ab6568d2c844d29253c1a437efaa171a3c9a7d1
Bryant <90587911+veD-tnayrB@users.noreply.github.com>
2024-01-02 09:22:49 -0400

    Remove console.log.

--- testing/modules/home/ts/views/index.tsx
+++ testing/modules/home/ts/views/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Api } from '@bgroup/http-suite/api';
-console.log("🚀 ~ file: index.tsx:3 ~ Api:", Api)
+
 export /*bundle*/
 	function View(): JSX.Element {
 	return (
